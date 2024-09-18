a10e0ca588f1c0ede1dcd4450c4fed0db9269a4c
richhost <hexcy@pm.me>
2024-09-18 15:44:51 +0800

    text stroke

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
 import { Link } from "@remix-run/react";
+import { SiGithub } from "@icons-pack/react-simple-icons";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,9 +15,12 @@ export default function Index() {
       <h1 className="text-center text-lg font-medium">
         Framer Motion Collections
       </h1>
-      <div className="border rounded-3xl max-w-xs w-full p-4 m-4">
+      <div className="border rounded-xl max-w-xs w-full p-4 m-4 flex flex-col gap-3">
         <Link to="/vision-pro" className="underline underline-offset-2">
           Vision Pro
+        </Link>
+        <Link to="/text-stroke" className="underline underline-offset-2">
+          Text Stroke
         </Link>
       </div>
       <p>
@@ -26,20 +30,7 @@ export default function Index() {
           target="_blank"
           rel="noreferrer"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-            <path d="M9 18c-4.51 2-5-2-7-2" />
-          </svg>
+          <SiGithub />
         </a>
       </p>
     </div>
---
+++ app/routes/text-stroke.tsx
@@ -0,0 +1,63 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import { Mouse, MoveDown } from "lucide-react";
+import { type CSSProperties, useRef } from "react";
+
+export default function Page() {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end end"],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 0.7], [1, 0.5]);
+  const opacity = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.6, 0.7],
+    [0.1, 1, 0.4, 0.1]
+  );
+  const contentX = useTransform(scrollYProgress, [0, 0.7], ["100%", "-400%"]);
+  const centerOpacity = useTransform(scrollYProgress, [0.5, 0.7], [0, 1]);
+
+  return (
+    <motion.main
+      ref={ref}
+      className="h-[500vh] bg-neutral-950 text-neutral-100 w-screen overflow-clip"
+      style={
+        {
+          "--scale": scale,
+          "--opacity": opacity,
+        } as CSSProperties
+      }
+    >
+      <div className="fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-[300px] md:h-[500px] flex items-center justify-center aspect-square after:absolute after:inset-0 after:border-[30px] after:border-sky-500 after:scale-[var(--scale)] before:absolute before:inset-0 before:border-[30px] before:border-transparent before:scale-[var(--scale)] before:z-20 after:border-opacity-[var(--opacity)]">
+        <motion.p
+          className="text-5xl md:text-9xl z-10 font-bold absolute top-1/2 left-full whitespace-nowrap"
+          aria-hidden
+          style={{ x: contentX, y: "-50%" }}
+        >
+          Best of luck
+        </motion.p>
+        <motion.p
+          className="text-5xl md:text-9xl z-20 font-bold absolute top-1/2 left-full whitespace-nowrap text-transparent [-webkit-text-stroke:1px_#f5f5f5]"
+          style={{ x: contentX, y: "-50%" }}
+        >
+          Best of luck
+        </motion.p>
+
+        <motion.h2
+          className="text-5xl font-bold relative z-20"
+          style={{ opacity: centerOpacity }}
+        >
+          Code By Abiee.
+        </motion.h2>
+
+        <span className="absolute top-0 w-screen h-full bg-neutral-950 z-10 left-[calc(50%*var(--scale)+50%)] origin-left scale-[var(--scale)]"></span>
+      </div>
+
+      <div className="fixed py-4 bottom-1 pointer-events-none flex justify-center left-0 w-full text-4xl font-bold">
+        <Mouse />
+        <MoveDown />
+      </div>
+    </motion.main>
+  );
+}
